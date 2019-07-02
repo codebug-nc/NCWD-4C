7e0d7bc7c1d6b92da87cde3c908632014147af79
McArthur Gill <mcarthur@storyfit.com>
2019-07-02 15:09:05 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', function() {
+//////////////////////////////////////////////////////////////////////////
+  // Write the `summertime` function below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function summertime() returns: ", summertime())
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `addFive` below. 
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function addFive(6) returns: ", addFive(10));
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `double` below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function double(6) returns: ", double(6));
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `greetMe` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function greetMe('Carl') returns: ", greetMe("Carl"));
+  // console.log("The function greetMe('Joe') returns: ", greetMe("Joe"));
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `isItLessThanTen` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log("The function isItLessThanTen(3) returns: ", isItLessThanTen(3));
+  // console.log("The function isItLessThanTen(20) returns: ", isItLessThanTen(20));
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `inHighSchool` below.
+
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log("The function inHighSchool(17) returns: ", inHighSchool(17));
+  // console.log("The function inHighSchool(5) returns: ", inHighSchool(5));
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `areTheySpeeding` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log("The function areTheySpeeding(60) returns: ", areTheySpeeding(60));
+  // console.log("The function areTheySpeeding(90) returns: ", areTheySpeeding(90));
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `sum` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log("The function sum(3, 4) returns: ", sum(3, 4));
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `product` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log("The function product(8, 10) returns: ", product(8, 10));
+//////////////////////////////////////////////////////////////////////////
+});
